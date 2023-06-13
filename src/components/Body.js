55de0895270b53ef50b001affbe07db5c4e55c22
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-06-14 00:22:01 +0300

    Add the jsx and the first draft style with grid

---
+++ src/components/Body.js
@@ -0,0 +1,22 @@
+import Button from './Button.js';
+
+function Body() {
+  return (
+    <div className="body">
+      <Button name="AC" />
+      <Button name="+/-" />
+      <Button name="%" />
+      <Button name={7} />
+      <Button name={8} />
+      <Button name={9} />
+      <Button name={4} />
+      <Button name={5} />
+      <Button name={6} />
+      <Button name={1} />
+      <Button name={2} />
+      <Button name={3} />
+
+    </div>
+  );
+}
+export default Body;
---
+++ src/components/Calculator.js
@@ -0,0 +1,20 @@
+import Button from './Button';
+import Body from './Body';
+
+function Calculator() {
+  return (
+    <div className="container">
+      <Button name={0} />
+      <Body />
+      <Button name="/" />
+      <Button name="x" />
+      <Button name="-" />
+      <Button name="+" />
+      <Button name="=" />
+      <Button name="0" />
+
+    </div>
+
+  );
+}
+export default Calculator;
