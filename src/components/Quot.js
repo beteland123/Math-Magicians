b74cd833a127f966792a4dec1b415a9a996e8f37
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-06-16 13:47:24 +0300

    Add loading state and error state

--- src/components/Quot.js
+++ src/components/Quot.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 
 function Quate() {
   const [quate, setQuate] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
     const fetchquate = async () => {
       try {
@@ -13,13 +15,21 @@ function Quate() {
         const data = await response.json();
 
         setQuate(data[0], data[1]);
+        setLoading(false);
       } catch (error) {
-        setQuate('Error fetching quote');
+        setError('Error fetching quote');
+        setLoading(false);
       }
     };
 
     fetchquate();
   }, []);
+  if (loading) {
+    return (<p>loading ...</p>);
+  }
+  if (error !== null) {
+    return (<p>{error}</p>);
+  }
   return (
     <div>
       <h2>Quate for You</h2>
