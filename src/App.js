dcd5288966814746a747c9e1d65a4154209e4b4f
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-06-21 12:15:22 +0300

    Add route for each page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './App.css';
 import Quate from './components/Quot';
+import Home from './components/Homepage';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quate />
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Quote" element={<Quate />} />
+      </Routes>
+    </>
   );
 }
 
